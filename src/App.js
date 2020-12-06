3b50d4dff21ca75d45d8d4a1cd2bca5f5a1e8080
Bruno Bortagaray <bruno.bortagaray@justwatch.com>
2020-12-06 16:55:11 +0100

    webpack com loaders para css e arquivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
+import api from "./services/api";
+
+import "./App.css";
+import backgroundImage from "./assets/background.jpeg";
 
 function App() {
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de App",
+    "Front-end web",
+  ]);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  }
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Homepage"></Header>
+
+      <img width={300} src={backgroundImage} />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
